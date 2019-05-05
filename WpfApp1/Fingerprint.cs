6061242d60c416cb54745379b09b3438978bedf3
Artur Tagisow <atagisov@gmail.com>
2019-05-05 13:24:07 +0200

    Mom i fixed it - skeletonization is still a bit wonky

--- WpfApp1/Fingerprint.cs
+++ WpfApp1/Fingerprint.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿#define DEBUG
+
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace WpfApp1
 {
@@ -16,19 +19,15 @@ namespace WpfApp1
         {
             xsize = (short)bm.Width;
             ysize = (short)bm.Height;
+            Console.WriteLine("Initialize(), width:" + xsize + " ysize: " + ysize);
             //all black pixels get marked as "1" in the array, white are "0"
             var kmmLevels = new short[bm.Width, bm.Height]; //filled with 0
-            for (int i = 0; i < bm.Width; i++)
+            for (int i = 1; i < bm.Width - 1; i++)
             {
-                for (int j = 0; j < bm.Height; j++)
+                for (int j = 1; j < bm.Height - 1; j++)
                 {
-                    //remove borders
-                    if (i == 0 || i == bm.Width - 1 || j == 0 || j == bm.Height - 1)
-                    {
-                        kmmLevels[i, j] = 0;
-                        continue;
-                    }
-                    if (bm.GetPixel(i, j) == Color.Black)
+                    var black = Color.FromArgb(255, 0, 0, 0);
+                    if (bm.GetPixel(i, j) == black)
                     {
                         kmmLevels[i, j] = 1;
                     }
@@ -36,7 +35,6 @@ namespace WpfApp1
             }
             kmmArray = kmmLevels;
         }
-
         private static bool isPixelCornersTouchZero(int pixelX, int pixelY)
         {
             //Pozostałe piksele 1, które posiadają sąsiadów o oznaczeniu 0 na rogach, oznaczamy jako 3.
@@ -62,7 +60,7 @@ namespace WpfApp1
                 var right = kmmArray[pixelX + 1, pixelY];
 
                 var up = kmmArray[pixelX, pixelY - 1];
-                var down = kmmArray[pixelX, pixelY - 1];
+                var down = kmmArray[pixelX, pixelY + 1];
 
                 if (left == 0 || right == 0 || up == 0 | down == 0) return true;
             }
@@ -95,7 +93,7 @@ namespace WpfApp1
                 {
                     //(2 of 2)... za pomocą maski sprawdzarka obliczamy ich wagę.
                     var pixel = kmmArray[pixelX + k, pixelY + l];
-                    short currentMaskPositionWeight = FingerprintConstants.checkerMask[k, l];
+                    short currentMaskPositionWeight = FingerprintConstants.checkerMask[k+1, l+1];
                     if (pixel > 0)
                         weight += currentMaskPositionWeight;
                 }
@@ -103,24 +101,42 @@ namespace WpfApp1
             return weight;
         }
 
-        private static short[,] addFours()
+        private static void addFours()
         {
+
             short[,] checkerMask = FingerprintConstants.checkerMask;
             short[,] result = kmmArray;
 
-            for (int i = 1; i < xsize; i++)
+            for (int i = 1; i < xsize-1; i++)
             {
-                for (int j = 1; j < ysize; j++)
+                for (int j = 1; j < ysize-1; j++)
                 {
                     //(1 of 2)Dla pikseli oznaczonych jako 2 ...
                     if (kmmArray[i, j] != 2) continue;
                     short weight = getWeight(i, j);
+                    Console.Write(weight+"");
                     // Jeśli waga znajduje się na liście czwórki oznaczenie piksela zamieniamy z 2 na 4.
                     if (FingerprintConstants.fourths.Contains(weight))
+                    {
                         result[i, j] = 4;
+                        Console.Write("!");
+                    }
                 }
+                Console.WriteLine();
             }
-            return result;
+            kmmArray = result;
+        }
+
+        private static void printArray()
+        {
+            for (int i = 0; i < xsize; i++)
+            {
+                for (int j = 0; j < ysize; j++)
+                {
+                    Console.Write(kmmArray[i,j]);
+                }
+                Console.WriteLine();
+            }
         }
 
         public static void cutOut()
@@ -144,23 +160,27 @@ namespace WpfApp1
             }
         }
 
-        private static bool compareMultidimensionalArrays(short[,] data1, short[,] data2)
+        private static bool arrayIdenticalToKmmArray(short[,] data)
         {
-            //i stole this from stackoverflow lmao
-            return data1.Rank == data2.Rank &&
-            Enumerable.Range(0, data1.Rank).All(dimension => data1.GetLength(dimension) == data2.GetLength(dimension)) &&
-            data1.Cast<short>().SequenceEqual(data2.Cast<short>());
+            for (int i = 0; i < xsize; i++)
+            {
+                for (int j = 0; j < ysize; j++)
+                {
+                    if (data[i, j] != kmmArray[i, j]) return false;
+                }
+            }
+            return true;
         }
 
         private static Bitmap kmmArrayToBitmap()
         {
-            var bm = new Bitmap((int)xsize, (int)ysize);
+            var bm = new Bitmap(xsize, ysize);
             for (int i = 0; i < xsize; i++)
             {
                 for (int j = 0; j < ysize; j++)
                 {
                     if (kmmArray[i, j] > 1) throw new Exception(); //to jest zly exception ale nie chce mi sie nowego tworzyc
-                    var color = kmmArray[i, j] == 0 ? Color.Black : Color.White;
+                    var color = kmmArray[i, j] == 0 ? Color.White : Color.Black;
                     bm.SetPixel(i, j, color);
                 }
             }
@@ -169,20 +189,29 @@ namespace WpfApp1
 
         public static Bitmap KMM(Bitmap bm)
         {
-            var tmp = Binarization.binarization(new Bitmap(bm), 255 / 2);
+            var tmp = Binarization.binarization(new Bitmap(bm), 127);
             //Wszystkie czarne piksele oznaczamy jako 1, zaś białe jako 0.
             initialize(tmp); //contains 0 and 1
 
+            int counter = 0;
             short[,] saveInitial;
             do
             {
-                saveInitial = kmmArray;
+                counter++;
+                saveInitial = (short[,]) kmmArray.Clone();
                 //Piksele 1, które posiadają sąsiadów o oznaczeniu 0 po bokach, u góry lub u dołu, oznaczamy jako 2.
                 //Pozostałe piksele 1, które posiadają sąsiadów o oznaczeniu 0 na rogach, oznaczamy jako 3.
                 runEdgeAndCornerCheck(); //contins 0,1,2,3
-
+#if DEBUG
+                Console.WriteLine("After runEdgeAndCornerCheck() (should contain 0123)");
+                printArray();
+#endif
                 //Dla pikseli oznaczonych jako 2 za pomocą maski sprawdzarka obliczamy ich wagę. Jeśli waga znajduje się na liście czwórki oznaczenie piksela zamieniamy z 2 na 4.
                 addFours(); //contains 0,1,2,3,4
+#if DEBUG
+                Console.WriteLine("After addFours() (should contain 01234)");
+                printArray();
+#endif
 
 
                 /*
@@ -191,7 +220,23 @@ namespace WpfApp1
                  Dla wszystkich pikseli oznaczonych jako 3 wyliczamy wagę za pomocą maski sprawdzarka. Jeśli waga znajduje się na liście wycięcia, zamieniamy piksel na 0, zaś w przeciwnym razie zamieniamy go na 1.
                 */
                 cutOut(); //contains 0,1
-            } while (!compareMultidimensionalArrays(kmmArray, saveInitial)); //powtarzamy dopoki operacja wywoluje zmiany
+
+#if DEBUG
+                Console.WriteLine("After cutOut() (should be only 0 and 1)");
+                printArray();
+
+                Console.WriteLine("Comparing current image to previous...");
+                if(arrayIdenticalToKmmArray(saveInitial))
+                {
+                    Console.WriteLine("No change since last iteration - EXITING");
+                }
+                else
+                {
+                    Console.WriteLine("Current and last iteration are different - RUNNING NEW ITERATION");
+                }
+#endif
+            } while (!arrayIdenticalToKmmArray(saveInitial)); //powtarzamy dopoki operacja wywoluje zmiany
+            Console.WriteLine("KMM Finished after " + counter + " iterations");
             return kmmArrayToBitmap();
         }
     }
